600cba602fcd91ae7b97259794c498427869529f
jerry <jbjoylab@gmail.com>
2024-07-13 17:56:39 +0900

    메뉴바액션추가

--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -1,16 +1,48 @@
 
+import { useState } from "react";
+import styled from "styled-components";
+
+const ToggleContainer = styled.div`
+   
+        position: fixed;
+        z-index: 5;
+        border-radius: 30px;
+        background-color: #e7e7e7;
+        width: 50%;
+        left: -50%;
+        transition: 0.5s ease;
+        &.open {
+            left: 50%;
+            transition: 0.5s ease;
+        }
+   
+
+
+    `
+    ;
 
 const MenuPage = () => {
 
-    return (
-        <div className="">
-            <li>Menu1</li>
-            <li>Menu2</li>
-            <li>Menu3</li>
-            <li>Menu4</li>
-            <li>숨기기</li>
-        </div>
+    const [isOn, setIsOn] = useState(true);
 
+    const toggleHandler = () => {
+        setIsOn(!isOn)
+    }
+
+    return (
+        <>
+        <ToggleContainer className={`${isOn ? "open" : ""}`}>
+                <ul >
+                    <li>Menu1</li>
+                    <li>Menu2</li>
+                    <li>Menu3</li>
+                    <li>Menu4</li>
+                </ul>
+        </ToggleContainer>
+        <div className="btn-container" onClick={toggleHandler}>보이기/숨기기</div>
+
+        
+        </>
     )
 }
 
